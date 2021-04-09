6c203c74823c2a1aec7a94b04c32b0a355895c17
jacques-svg <66543477+jacques-svg@users.noreply.github.com>
2021-04-09 12:48:19 -0300

    first

--- App.js
+++ App.js
@@ -1,14 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , { Component } from 'react'
+import { StyleSheet, Text, View,TextInput,Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export class  App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        email :'',
+        password:'',
+        name:''
+    }
+}
+
+  async logInFB() {
+    try {
+      await Facebook.initializeAsync({
+        appId: '120565410055659',
+      });
+      const {
+        type,
+        token,
+        expirationDate,
+        permissions,
+        declinedPermissions,
+      } = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ['public_profile'],
+      });
+      if (type === 'success') {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+        Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  }
+  render(){
+
+    return (
+      <View>
+        <TextInput
+          placeholder="name"
+          onChangeText={(name) => this.setState({ name })}
+        />
+        <TextInput
+          placeholder="email"
+          onChangeText={(email) => this.setState({ email })}
+        />
+        <TextInput
+          placeholder="password"
+          secureTextEntry={true}
+          onChangeText={(password) => this.setState({ password })}
+        />
+        <Button onPress={() => this.logInFB.bind(this)} title="Log" />
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +69,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
